3126328e88342dfd242dd344c368a22c53790fdc
nat <nsebcarps09@gmail.com>
2020-07-20 16:17:10 +0800

    chore(strapi_backend_tests): Add createProduct integ test

--- __tests__/integ-graphql.test.ts
+++ __tests__/integ-graphql.test.ts
@@ -1,16 +1,26 @@
 import { createTestClient } from 'apollo-server-testing'
 import gql from 'graphql-tag'
 import { constructTestServer } from './__utils'
+import { createProduct } from '../src/helper'
 
 const { server }: any = constructTestServer({
   context: {
+    //Product
     createProduct: jest.fn(() => async (product) => {
-      return { createProduct: { id: '1' } }
+      console.log('createProducaegr', product)
+      return { data: { id: '1', ...product } }
+    }),
+    updateProduct: jest.fn(() => async (product) => {
+      return { updateProduct: { id: '1' } }
+    }),
+    deleteProduct: jest.fn(() => async (product) => {
+      return { deleteProduct: { id: '1' } }
     }),
   },
 })
 
 describe('Queries', () => {
+  //Mutations
   it('create a product', async () => {
     const CREATE_PRODUCT = gql`
       mutation a($input: createProductInput) {
@@ -57,7 +67,6 @@ describe('Queries', () => {
             amount: '123',
             productCode: '420',
             quantity: '100',
-            Image: '1',
           },
         },
       },
@@ -72,9 +81,58 @@ describe('Queries', () => {
         amount: '123',
         productCode: '420',
         quantity: '100',
-        Image: '1',
       },
     })
     expect(res).toMatchSnapshot()
   })
+
+  // it('delete a product', async () => {
+  //   const DELETE_PRODUCT = gql`
+  //     mutation a($input: deleteProductInput) {
+  //       deleteProduct(input: $input) {
+  //         product {
+  //           id
+  //           created_at
+  //           updated_at
+  //           name
+  //           description
+  //           amount
+  //           productCode
+  //           quantity
+  //           Image {
+  //             id
+  //             created_at
+  //             updated_at
+  //             name
+  //             caption
+  //             alternativeText
+  //             width
+  //             height
+  //             hash
+  //             ext
+  //             mime
+  //             size
+  //             url
+  //             provider
+  //             previewUrl
+  //           }
+  //         }
+  //       }
+  //     }
+  //   `
+
+  //   const { mutate } = createTestClient(server)
+  //   const res = await mutate({
+  //     mutation: DELETE_PRODUCT,
+  //     variables: {
+  //       input: {
+  //         where: {
+  //           id: '1',
+  //         },
+  //       },
+  //     },
+  //   })
+
+  //   expect(res).toMatchSnapshot()
+  // })
 })
