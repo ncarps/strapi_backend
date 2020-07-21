ae5b593e9cf4b4dc3d68171bb252634a04082be3
nat <nsebcarps09@gmail.com>
2020-07-21 10:48:15 +0800

    chore(strapi-integ-test): Add updateProduct integ test

--- __tests__/integ-graphql.test.ts
+++ __tests__/integ-graphql.test.ts
@@ -5,16 +5,41 @@ import { createProduct } from '../src/helper'
 
 const { server }: any = constructTestServer({
   context: {
-    //Product
     createProduct: jest.fn(() => async (product) => {
-      console.log('createProducaegr', product)
-      return { data: { id: '1', ...product } }
-    }),
-    updateProduct: jest.fn(() => async (product) => {
-      return { updateProduct: { id: '1' } }
+      return {
+        data: {
+          createProduct: {
+            product: {
+              id: '1',
+              ...product.data,
+            },
+          },
+        },
+      }
     }),
     deleteProduct: jest.fn(() => async (product) => {
-      return { deleteProduct: { id: '1' } }
+      return {
+        data: {
+          deleteProduct: {
+            product: {
+              id: '1',
+              ...product.data,
+            },
+          },
+        },
+      }
+    }),
+    updatedProduct: jest.fn(() => async (product) => {
+      return {
+        data: {
+          createProduct: {
+            product: {
+              id: '1',
+              ...product.data,
+            },
+          },
+        },
+      }
     }),
   },
 })
@@ -55,6 +80,43 @@ describe('Queries', () => {
       },
     })
 
+    expect(res).toMatchSnapshot()
+  })
+
+  it('update a product', async () => {
+    const UPDATE_PRODUCT = gql`
+      mutation a($input: createProductInput) {
+        createProduct(input: $input) {
+          product {
+            id
+            created_at
+            updated_at
+            name
+            description
+            amount
+            productCode
+            quantity
+          }
+        }
+      }
+    `
+
+    const { mutate } = createTestClient(server)
+    const res = await mutate({
+      mutation: UPDATE_PRODUCT,
+      variables: {
+        input: {
+          data: {
+            name: 'HOTDOG',
+            description: 'Hotdog numbawan',
+            amount: '123',
+            productCode: '420',
+            quantity: '100',
+          },
+        },
+      },
+    })
+
     expect(res.errors).toBeUndefined()
     expect(res.data).toMatchObject({
       createProduct: {
@@ -71,53 +133,53 @@ describe('Queries', () => {
     expect(res).toMatchSnapshot()
   })
 
-  // it('delete a product', async () => {
-  //   const DELETE_PRODUCT = gql`
-  //     mutation a($input: deleteProductInput) {
-  //       deleteProduct(input: $input) {
-  //         product {
-  //           id
-  //           created_at
-  //           updated_at
-  //           name
-  //           description
-  //           amount
-  //           productCode
-  //           quantity
-  //           Image {
-  //             id
-  //             created_at
-  //             updated_at
-  //             name
-  //             caption
-  //             alternativeText
-  //             width
-  //             height
-  //             hash
-  //             ext
-  //             mime
-  //             size
-  //             url
-  //             provider
-  //             previewUrl
-  //           }
-  //         }
-  //       }
-  //     }
-  //   `
+  it('delete a product', async () => {
+    const DELETE_PRODUCT = gql`
+      mutation a($input: deleteProductInput) {
+        deleteProduct(input: $input) {
+          product {
+            id
+            created_at
+            updated_at
+            name
+            description
+            amount
+            productCode
+            quantity
+            Image {
+              id
+              created_at
+              updated_at
+              name
+              caption
+              alternativeText
+              width
+              height
+              hash
+              ext
+              mime
+              size
+              url
+              provider
+              previewUrl
+            }
+          }
+        }
+      }
+    `
 
-  //   const { mutate } = createTestClient(server)
-  //   const res = await mutate({
-  //     mutation: DELETE_PRODUCT,
-  //     variables: {
-  //       input: {
-  //         where: {
-  //           id: '1',
-  //         },
-  //       },
-  //     },
-  //   })
+    const { mutate } = createTestClient(server)
+    const res = await mutate({
+      mutation: DELETE_PRODUCT,
+      variables: {
+        input: {
+          where: {
+            id: '1',
+          },
+        },
+      },
+    })
 
-  //   expect(res).toMatchSnapshot()
-  // })
+    expect(res).toMatchSnapshot()
+  })
 })
